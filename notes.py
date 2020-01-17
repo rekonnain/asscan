ef2063700e350968f8686e7072ba516efb01274d
osku <ossi@disobey.fi>
2020-01-17 15:39:06 +0200

    add the actual notes python file

---
+++ notes.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python3
+import sys,json
+
+filename='notes.json'
+
+def notesforhost(hostname):
+    j = {}
+    try:
+        j = json.loads(open(filename,'r').read())
+    except:
+        pass
+    if hostname in j:
+        return {hostname: j[hostname]}
+    else:
+        return {}
+
+def savenote(host, note):
+    j = {}
+    try:
+        j = json.loads(open(filename,'r').read())
+    except:
+        pass
+    j[host] = note
+    open(filename,'w').write(json.dumps(j, sort_keys = True, indent = 4))
+
+def deletenote(host):
+    j = {}
+    try:
+        j = json.loads(open(filename,'r').read())
+        del j[host]
+        open(filename,'w').write(json.dumps(j, sort_keys = True, indent = 4))
+    except:
+        pass
+    
+def hostswithcomments():
+    try:
+        j = json.loads(open(filename,'r').read())
+        return j.keys()
+    except:
+        return []
+    
